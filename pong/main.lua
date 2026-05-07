c09213dc454fdabf8a803fdaa80ba9300339d856
Farshad <farshad.dev.contact@gmail.com>
2026-05-07 15:22:26 +0330

    ✨ Feat: Pong 2

--- pong/main.lua
+++ pong/main.lua
@@ -7,8 +7,14 @@ VIRTUAL_HEIGHT = 243
 push = require 'push'
 
 function love.load()
-    love.graphics.getDefaultFilter('nearest', 'nearest')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    largeFont = love.graphics.newFont('font.ttf', 32)
 
+    player1Score = 0
+    player2Score = 0
+    
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true,
@@ -27,7 +33,17 @@ end
 function love.draw()
     push.start()
 
-    love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+
+    love.graphics.setFont(largeFont)
+
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 80)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 50, VIRTUAL_HEIGHT / 2 - 80)
+
+    love.graphics.rectangle('fill', 10, 10, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push.finish()
 end
